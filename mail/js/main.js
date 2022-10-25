1c9324f31acbd2ecede7a15790e5c93dfcebc058
Vale-riad <valentinariad91@gmail.com>
2022-10-25 19:17:47 +0200

    corregge esercizio

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,13 +1,13 @@
-const emails = ["vale.riad.91@gmail.com" , "valentina-riad-91@gmail.com", "vale-91-riad@gmai.com"]
+const emails = ["vale.riad.91@gmail.com" , "valentina-riad-91@gmail.com", "vale-91-riad@gmai.com"];
 // 1. chiedere all'utente la sua email
 
 const email = prompt("Inserisci la tua e-mail");
 
-if(emails.indexOf("email") == 0)  
+if(emails.indexOf("email") !== 0)  
 {  
         alert("Yes, the value exists!")  
 }   
 else  
 {  
         alert("No, the value is absent.")  
-} 
+}
